51e2de519c08ea546840237cf94a7d075bbdc8bc
Oleg Artyomov <oleg.artomov@pragmaticplay.com>
2023-10-26 08:19:31 -0400

    Code cleanup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# MegaFairTestTask
+How to start:
+  1. Run docker container with Postgres
+     docker run --name megafair-pg -p 5432:5432 -e POSTGRES_USER=megafair -e POSTGRES_PASSWORD=olga1999 -e POSTGRES_DB=megafairdb -d postgres:13.3
--- src/main/java/com/megafair/auth/web/GameResource.java
+++ src/main/java/com/megafair/auth/web/GameResource.java
@@ -1,8 +1,7 @@
 package com.megafair.auth.web;
 
 import jakarta.annotation.security.RolesAllowed;
-import jakarta.enterprise.context.RequestScoped;
-import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -11,23 +10,23 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.SecurityContext;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 
+import java.security.Principal;
+
 import static com.megafair.auth.utils.Constants.GAME_ID_CLAIM_NAME;
 import static com.megafair.auth.utils.Constants.PLATFORM_ID_CLAIM_NAME;
 import static com.megafair.auth.utils.Constants.USER_ID_CLAIM_NAME;
 import static com.megafair.auth.utils.Constants.USER_ROLE;
 
 @Path("/game")
-@RequestScoped
+@Singleton
 public class GameResource {
-
-    @Inject
-    JsonWebToken jwt;
 
     @POST
     @Path("makeBet")
     @RolesAllowed({USER_ROLE})
     @Produces(MediaType.TEXT_PLAIN)
     public String makeBet(@Context SecurityContext ctx) {
+        JsonWebToken jwt = (JsonWebToken) ctx.getUserPrincipal();
         String info = "User: " + jwt.getClaim(USER_ID_CLAIM_NAME) +
                 ", platform_id: " + jwt.getClaim(PLATFORM_ID_CLAIM_NAME) +
                 ", game_id: " + jwt.getClaim(GAME_ID_CLAIM_NAME);
